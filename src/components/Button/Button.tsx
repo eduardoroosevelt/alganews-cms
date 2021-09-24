16b16a47a974d03cd9e9bd323d8491df9aff2987
eduardoroosevelt <eduardo.roosevelt.de.oliveira@gmail.com>
2021-09-24 16:19:49 -0400

    primeiro Commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import * as B from './Button.styles'
+
+
+export interface ButtonProps {
+  variant: 'danger' | 'primary' | 'text',
+  label: string
+}
+
+export default function Button({ label, variant }: ButtonProps) {
+  return <B.Wrapper variant={variant}>
+    {label}
+  </B.Wrapper>
+}
