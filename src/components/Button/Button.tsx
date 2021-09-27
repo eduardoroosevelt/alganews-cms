a5dc37ecc6d77ce9ed84c722929ac31ddf39bbba
eduardo <eduardo.roosevelt.de.oliveira@gmail.com>
2021-09-27 10:57:01 -0400

    Instalando o polished e estilisando o botão disabled

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,17 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react'
 import * as B from './Button.styles'
 
 
-export interface ButtonProps {
+export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   variant: 'danger' | 'primary' | 'text',
-  label: string
+  label: string,
 }
 
-export default function Button({ label, variant }: ButtonProps) {
-  return <B.Wrapper variant={variant}>
+export default function Button({ label, variant, ref, ...props }: ButtonProps) {
+  return <B.Wrapper
+    variant={variant}
+    {...props}
+  >
     {label}
   </B.Wrapper>
 }
