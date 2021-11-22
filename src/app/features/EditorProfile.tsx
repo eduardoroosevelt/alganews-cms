9e6ba87029cb14f65daad08df30ff58d1b4364f3
eduardo <eduardo.roosevelt.de.oliveira@gmail.com>
2021-11-22 09:45:40 -0400

    error boundary

--- src/app/features/EditorProfile.tsx
+++ src/app/features/EditorProfile.tsx
@@ -9,6 +9,9 @@ interface EditorProfileProps {
 }
 
 function EditorProfile(props: EditorProfileProps) {
+
+  throw new Error("Houve um erro ao renderizar o componente Editor Profile");
+
   return <EditorProfileWrapper>
     <EditorHeadline>
       <Avatar src={'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'} />
